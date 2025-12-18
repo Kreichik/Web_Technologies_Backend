b3368feb20019c8f8d46a14555998643503b37b2
Yan <kudashovian2006@gmail.com>
2025-12-18 14:37:45 +0500

    implement POST, PUT, DELETE methods. Create Readme file

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const fs = require('fs');
 const dbPath = './data.json';
+const { v4: uuidv4 } = require('uuid');
 
 let app = express();
 
 app.use(express.json());
 
+function readData(){
+    const data = fs.readFileSync(dbPath, 'utf-8');
+    return JSON.parse(data);
+}
+
+function writeData(data){
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+}
+
 app.get('/', (request, response) => {
     response.send('Server is running');
-    // response.sendFile(__dirname + '/index.html');
 })
 
 app.get('/hello', (request, response) => { 
@@ -29,15 +38,72 @@ app.get('/tickets', (request, response) => {
             response.status(500).send('Error reading data');
             return;
         }
-        response.json(JSON.parse(data));
+        data = readData();
+        response.json(data);
     })
 })
 
 app.post('/tickets', (request, response) => {
     fs.readFile(dbPath, 'utf-8', (err, data) => {
-        
+        if (err) {
+            response.status(500).send('Error reading data');
+            return;
+        }
+
+        const dbData = readData();
+        const newTicket = { id: uuidv4(),
+            name: request.body.name,
+            price: request.body.price,
+            date: request.body.date
+         };
+
+        dbData.tickets.push(newTicket);
+
+        writeData(dbData);
+
+        response.status(201).json(newTicket);
+     
     })
 })
+
+app.put('/tickets/:id', (request, response) => {
+    if (!request.params.id) {
+        response.status(400).send('Ticket ID is required');
+        return;
+    }
+
+    const dbData = readData();
+    const ticketIndex = dbData.tickets.findIndex(ticket => ticket.id === request.params.id);
+    if (ticketIndex === -1) {
+        response.status(404).send('Ticket not found');
+        return;
+    }
+    dbData.tickets[ticketIndex] = {
+        id: request.params.id,
+        ...request.body
+    };
+
+    writeData(dbData);
+    response.json(dbData.tickets[ticketIndex]);
+
+});
+
+app.delete('/tickets/:id', (request, response) => { 
+    if (!request.params.id) {
+        response.status(400).send('Ticket ID is required');
+        return;
+    }
+
+    const dbData = readData();
+    const ticketIndex = dbData.tickets.findIndex(ticket => ticket.id === request.params.id);
+    if (ticketIndex === -1) {
+        response.status(404).send('Ticket not found');
+        return;
+    }
+    dbData.tickets.splice(ticketIndex, 1);
+    writeData(dbData);
+    response.json({ success: 'true' });
+});
 
 app.listen(3000, () => {
     console.log("Server started at http://localhost:3000");
